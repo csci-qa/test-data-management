bd616d27321285fc4f6febd175c4499dfc801635
Colton M <colton.adkins56@yahoo.com>
2016-11-09 14:59:24 -0800

    test-data-management HW

--- features/page_classes/login_page.rb
+++ features/page_classes/login_page.rb
@@ -1,7 +1,9 @@
 require 'page-object'
+require 'faker'
 
 class LoginPage
   include PageObject
+  include DataMagic
 
   page_url "file:///#{Dir.pwd}/features/fakewebsite/login.html"
 
@@ -9,10 +11,19 @@ class LoginPage
   text_field(:password, name: 'pass')
   button(:login, name: 'login')
 
+  def populate_page
+    populate_page_with data_for :my_page
+  end
+
   def login_with_valid_credentials
     self.username = "Bob"
     self.password = "password"
     self.login
   end
 
+  def login_with_invalid_credentials
+    self.username = Faker::Name.first_name
+    self.password = nil
+    self.login
+  end
 end
